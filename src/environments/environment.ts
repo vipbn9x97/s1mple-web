d2919c1c7b57da425da82bba968e92a6e8fcb668
vipbn9x97 <vanhach97@gmail.com>
2020-06-16 17:46:53 +0700

    initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyARrFcfRN-Wn_Ln44JU70q27zWZ6OMg-js',
+    authDomain: 's1mple-web.firebaseapp.com',
+    databaseURL: 'https://s1mple-web.firebaseio.com',
+    projectId: 's1mple-web',
+    storageBucket: 's1mple-web.appspot.com',
+    messagingSenderId: '728229334200',
+    appId: '1:728229334200:web:cdab4012eebf39a4c7f84d',
+    measurementId: 'G-NJZCJ84H9W',
+  },
 };
 
 /*
